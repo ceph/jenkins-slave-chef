e1746cd06394f748600e94db543edc6581ee3b38
Sandon Van Ness <sandon@inktank.com>
2014-09-08 10:08:29 -0700

    Remove copied folder which shouldn't have been there.
    Signed-off-by: Sandon Van Ness <sandon@inktank.com>

--- cookbooks/gitbuilder-cdep-deb-cloud-trusty-amd64-basic/jenkins-slave-chef/solo/solo.rb
+++
@@ -1,6 +0,0 @@
-#root = File.absolute_path(File.dirname(__FILE__))
-root = File.expand_path(File.dirname(__FILE__))
-
-file_cache_path root
-cookbook_path root + '/../cookbooks'
-data_bag_path root + '/../data_bags'
